6f26ef2ba7b7cd08ff65307fa57a489747fdf40f
Julio Brazao <jotabrazao@gmail.com>
2023-09-12 20:18:43 -0400

    Added a button without any magic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import Main from "./components/Main";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  background: #aaffaa;
+  color: #222;
+  font-weight: bold;
+  &:hover {
+    background: #bbffcc;
+    color: #444;
+  }
+  &:active {
+    background: #fff;
+    color: #000;
+  }
+`;
 
 export default function App() {
+
+  const handleClick = () => {
+    alert('You clicked!');
+  }
+
   return (
-    <> 
-      <Main />
+    <>
+      <div className="container-fluid mt-3 text-center">
+        <StyledButton className="btn btn-primary border-0" onClick={handleClick}>
+          Click me!
+        </StyledButton> 
+      </div>
     </>
   );
 }
